cd6e59053e2eb67d6240c673acdb59d40621463e
cruzguille <guillermobelizcruz98@gmail.com>
2025-08-07 17:52:09 -0300

    subiendo backend express

--- backend/index.js
+++ backend/index.js
@@ -6,16 +6,17 @@ const PORT = process.env.PORT || 3001;
 
 // Este bloque mantiene viva la conexión con la base de datos
 setInterval(async () => {
-  const client = await db.pool.connect();
   try {
-    await client.query('SELECT 1');
+    await db.query('SELECT 1'); // Ping a la base
     console.log('⏱️ Ping a la base de datos exitoso');
   } catch (error) {
     console.error('⚠️ Error en ping a la base de datos:', error);
-  } finally {
-    client.release();
   }
-}, 5 * 60 * 1000);
+}, 5 * 60 * 1000); // cada 5 minutos
+
+app.listen(PORT, () => {
+  console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
+});
 
 // Solo para debug del certificado
 console.log("🔒 Leyendo certificado:", fs.readFileSync('./src/afip/certs/certificado.pem', 'utf8'));
